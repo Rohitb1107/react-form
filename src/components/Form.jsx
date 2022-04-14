e2219ed867fb77415cd39c6ef2581312801e81ec
Rohit <stylishrb711@gmail.com>
2022-04-14 13:30:52 +0530

    input part done

---
+++ src/components/Form.jsx
@@ -0,0 +1,14 @@
+const Form = () => {
+  return (
+    <>
+      <form>
+        <input type="text" placeholder="Enter Username" id="username" />
+        <input type="number" placeholder="Enter Age" id="age" />
+
+        <input type="submit" value="Submit" />
+      </form>
+    </>
+  );
+};
+
+export default Form;
