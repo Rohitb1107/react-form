db3bc5217e16d3bdd9c8d34f5d0d1f683ac67f37
Rohit <stylishrb711@gmail.com>
2022-04-18 13:13:39 +0530

    form data

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,9 +1,44 @@
+import { useState } from "react";
+
 const Form = () => {
+  const [form, setForm] = useState({
+    username: "",
+    age: "",
+  });
+
+  const handleChange = (e) => {
+    if (e.target.id === "username") {
+      setForm({
+        ...form,
+        username: e.target.value,
+      });
+    }
+
+    if (e.target.id === "age") {
+      setForm({
+        ...form,
+        age: e.target.value,
+      });
+    }
+  };
+
   return (
     <>
       <form>
-        <input type="text" placeholder="Enter Username" id="username" />
-        <input type="number" placeholder="Enter Age" id="age" />
+        <input
+          onChange={handleChange}
+          value={form.username}
+          type="text"
+          placeholder="Enter Username"
+          id="username"
+        />
+        <input
+          onChange={handleChange}
+          value={form.age}
+          type="number"
+          placeholder="Enter Age"
+          id="age"
+        />
 
         <input type="submit" value="Submit" />
       </form>
